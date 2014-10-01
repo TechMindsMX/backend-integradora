674f5469fa7cad0afbfd5522e34b5af77fb4ece1
josdem <joseluis.delacruz@gmail.com>
2014-10-01 15:24:06 -0500

    #74 agregando archivos de gulp para las pruebas

---
+++ client/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'src',
+    port: 8091,
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('./src/**/*.js')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/**/*.js'], ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
