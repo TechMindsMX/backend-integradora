7baa6f6f8b1f7d0a2f50e44951d2734a7a504c2b
josdem <joseluis.delacruz@gmail.com>
2014-10-01 19:33:31 -0500

    #74 Modelo Product creado

---
+++ app/grails-app/domain/com/tim/one/integradora/Product.groovy
@@ -0,0 +1,31 @@
+package com.tim.one.integradora
+
+import grails.rest.*
+
+@Resource(uri="/products")
+class Product {
+
+  String name
+  String measure
+  BigDecimal price
+  BigDecimal iva
+  BigDecimal ieps
+  String currency
+  ProjectStatus status
+  String description
+
+  Date dateCreated    
+  Date lastUpdated
+
+  static belongsTo = [user:User]
+  static hasMany = [subProjects:Project]
+
+  static constraints = {
+    name blank:false,size:1..255
+    price min:0.00,scale:2
+    iva min:0.00,max:100.00,scale:2
+    ieps min:0.00,max:100.00,scale:2
+    description blank:true,nullable:true,size:1..255
+  }
+
+}
