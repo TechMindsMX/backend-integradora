504002ebdf49a4673002e50be50383be3ed7a041
josdem <joseluis.delacruz@gmail.com>
2014-10-02 17:00:48 -0500

    #74 agregando el método delete de para rest

--- app/grails-app/controllers/com/tim/one/integradora/UserController.groovy
+++ app/grails-app/controllers/com/tim/one/integradora/UserController.groovy
@@ -5,9 +5,11 @@ import grails.rest.*
 
 class UserController {
 
+  static allowedMethods = [index:'GET', save:'POST', delete:'DELETE']
+
   def index(Integer max) {
     params.max = Math.min(max ?: 10, 100)
-    respond User.list(params), model:[userCount: User.count()]
+    respond User.findAllByStatus(UserStatus.ENABLED, params), model:[userCount: User.count()]
   }
 
   @Transactional
@@ -18,6 +20,14 @@ class UserController {
     }
     user.save()
     respond user
+  }
+
+  @Transactional
+  def delete(Long id) {
+    User user = User.get(id)
+    user.status = UserStatus.DISABLED
+    user.save()
+    render(status:202)
   }
 
 }
