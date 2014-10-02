3c544bf9f952000d71b7063bdca5b25b392ad071
josdem <joseluis.delacruz@gmail.com>
2014-10-02 15:53:48 -0500

    #74 Creando UserController manualmente

--- client/src/main/user.js
+++ client/src/main/user.js
@@ -3,9 +3,21 @@ function User() {
   return {
     list: function() {
       var promise = new RSVP.Promise(function(resolve, reject) {
-        $.getJSON('http://localhost:8090/tim-integradora/users.json').
+        $.getJSON('http://localhost:8080/tim-integradora/users.json').
         done(function(data) {
-          resolve([1,2,3,4]);
+          resolve(data);
+        }).fail(function(error){
+          reject(error);
+        })
+      });
+
+      return promise;
+    },
+    save: function(user) {
+      var promise = new RSVP.Promise(function(resolve, reject) {
+        $.post('http://localhost:8080/tim-integradora/users?format=json', user).
+        done(function(data) {
+          resolve(data);
         }).fail(function(error){
           reject(error);
         })
--- client/src/test/userSpec.js
+++ client/src/test/userSpec.js
@@ -1,13 +1,41 @@
 describe("Users behavior", function() {
 
-  it("Should get an empty user list", function() {
-    var user = new User();
-    user.list().then(
-      function(data) {
-        console.log(data.length);
-        expect(10).toEqual(0);
-      }
-    );
+  describe("Saving an user", function() {
+    var newUser = null;
+
+    beforeEach(function(done) {
+      var user = new User();
+      user.save({name: 'usuario', email: 'usuario6@email.com'}).then(
+        function(data) {
+          newUser = data; // deserialize
+          done();
+        }
+      );
+    });
+
+    it("Should create an user", function(done) {
+      expect(newUser).not.toBe(null);
+      done();
+    });
+  });
+
+  describe("List behavior", function() {
+    var list = [];
+
+    beforeEach(function(done) {
+      var user = new User();
+      user.list().then(
+        function(data) {
+          list = data;
+          done();
+        }
+        );
+    });
+
+    it("Should get a list of all users", function(done) {
+      expect(list.length).toBeGreaterThan(0);
+      done();
+    });
   });
 
 });
