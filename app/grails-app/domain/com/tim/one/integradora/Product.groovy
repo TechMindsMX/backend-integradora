42431637b4654ee9f0ce2f21d78c2f7fded0419f
josdem <joseluis.delacruz@gmail.com>
2014-10-03 16:57:40 -0500

    #72 Agregando constraints y status por defecto

--- app/grails-app/domain/com/tim/one/integradora/Product.groovy
+++ app/grails-app/domain/com/tim/one/integradora/Product.groovy
@@ -2,17 +2,18 @@ package com.tim.one.integradora
 
 import grails.rest.*
 
-@Resource(uri="/products")
 class Product {
 
   String name
   String measure
+  String currency
+  String description
+
+  ProjectStatus status = ProjectStatus.ENABLED
+
   BigDecimal price
   BigDecimal iva
   BigDecimal ieps
-  String currency
-  ProjectStatus status
-  String description
 
   Date dateCreated    
   Date lastUpdated
@@ -25,6 +26,8 @@ class Product {
     iva min:0.00,max:100.00,scale:2
     ieps min:0.00,max:100.00,scale:2
     description blank:true,nullable:true,size:1..255
+    measure size:1..100, nullable:true
+    currency size:1..3
   }
 
 }
