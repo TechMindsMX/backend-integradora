994d73d7e61e4001cb1a148783a1c37e98d96f74
sohjiro <felipe@makingdevs.com>
2014-10-08 10:49:05 -0500

    coding #82 ajustando las pruebas por detalle del enabled

--- app/test/unit/com/tim/one/integradora/UserSpec.groovy
+++ app/test/unit/com/tim/one/integradora/UserSpec.groovy
@@ -20,68 +20,11 @@ class UserSpec extends Specification {
       user.id
       user.name == name
       user.email == email
-      user.status == UserStatus.ENABLED
+      user.enabled
 
     where:
       name    | email
       "name1" | "name@email.com"
-  }
-
-  def "Adding a relation to an existing user"() {
-    given: "An existing user"
-      def user = new User(name: "name", email: "email@user.com")
-      user.save()
-
-    and: "Add an provider"
-      def provider = new User(name:providerName, email:providerEmail, status: UserStatus.DISABLED)
-      provider.save()
-
-      def relationship = new Relationship(user: provider, type: relationshipType)
-      user.addToRelationships(relationship)
-
-    when:
-      def userUpdated = user.save()
-
-    then:
-      userUpdated.id
-      userUpdated.relationships.size() > 0
-      userUpdated.relationships[0].type == relationshipType
-      userUpdated.relationships[0].user.status == UserStatus.DISABLED
-
-    where:
-      providerName  | providerEmail        | relationshipType
-      "provider"    | "email@provider.com" | RelationshipType.PROVIDER
-      "client"      | "email@client.com"   | RelationshipType.CLIENT
-  }
-
-
-  def "Enable provider/client as an user"() {
-    given: "An existing user"
-      def user = new User(name: "name", email: "email@user.com")
-      user.save()
-
-    and: "Add an existing provider"
-      def provider = new User(name:providerName, email:providerEmail, status: UserStatus.DISABLED)
-      provider.save()
-
-      def relationship = new Relationship(user: provider, type: relationshipType)
-      relationship.save()
-      user.addToRelationships(relationship)
-      user.save()
-
-    and: "Enabling a provider"
-      provider.status = UserStatus.ENABLED
-
-    when:
-      def providerUpdated = provider.save()
-
-    then:
-      providerUpdated.status == UserStatus.ENABLED
-      !providerUpdated.relationships.isEmpty()
-
-    where:
-      providerName  | providerEmail        | relationshipType
-      "provider"    | "email@provider.com" | RelationshipType.PROVIDER
   }
 
   void "validating constraints"() {
