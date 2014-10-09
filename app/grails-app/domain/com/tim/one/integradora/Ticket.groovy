48f3e0c429bf7df55c6888e0f5ede38d7cdb647a
sohjiro <felipe@makingdevs.com>
2014-10-09 17:05:12 -0500

    coding #119 agregando tickets para la orden de depósito

---
+++ app/grails-app/domain/com/tim/one/integradora/Ticket.groovy
@@ -0,0 +1,29 @@
+package com.tim.one.integradora
+
+class Ticket {
+
+  String currency
+  String observations
+  String attachmentUrl
+  BigInteger orderNumber // auto-increment per integrated
+
+  User integrated
+
+  BigDecimal totalAmount
+
+  PaymentType paymentType = PaymentType.CASH
+  TicketStatus status = TicketStatus.NEW
+
+  Date paymentDate
+  Date dateCreated
+  Date lastUpdated
+
+  static constraints = {
+    currency size:1..3
+    observations size:1..255, nullable:true, blank:false
+    attachmentUrl size:1..255, nullable:true, blank: true
+    payment min:0.00, scale:2
+    totalAmount min:0.00, scale:2
+  }
+
+}
