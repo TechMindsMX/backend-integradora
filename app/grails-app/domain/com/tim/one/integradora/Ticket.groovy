38608ef08a99b1611fd1149c157a4b88c4871ead
sohjiro <felipe@makingdevs.com>
2014-10-10 12:34:24 -0500

    coding #119 removiendo código no necesario

--- app/grails-app/domain/com/tim/one/integradora/Ticket.groovy
+++ app/grails-app/domain/com/tim/one/integradora/Ticket.groovy
@@ -2,17 +2,14 @@ package com.tim.one.integradora
 
 class Ticket {
 
+  User integrated
+
   String currency
   String observations
-  String attachmentUrl
-  BigInteger orderNumber // auto-increment per integrated
-                         //esto no esta en la orden de compra asi que se debe quitar para usar esta tabla como base para las ordenes
-
-  User integrated
 
   BigDecimal totalAmount
 
-  PaymentType paymentType = PaymentType.CASH
+  PaymentType paymentType = PaymentType.CHECK
   TicketStatus status = TicketStatus.NEW
 
   Date paymentDate
@@ -22,9 +19,8 @@ class Ticket {
   static constraints = {
     currency size:1..3
     observations size:1..255, nullable:true, blank:false
-    attachmentUrl size:1..255, nullable:true, blank: true
-    payment min:0.00, scale:2
     totalAmount min:0.00, scale:2
+    paymentDate nullable:true
   }
 
 }
